8bd50d2c484e140de9d97896a35d3f3876a68c17
shiertier <junjie.text@gmail.com>
2024-11-08 11:35:12 +0800

    update

--- src/shiertier_logger/logger.py
+++ src/shiertier_logger/logger.py
@@ -1,59 +1,69 @@
 import os.path
-from os import makedirs
+from os import makedirs, environ
 from loguru import logger
 import sys
-from typing import Dict, Any
 
-__all__ = ["logger", "Logger_I18n", "easy_logger_i18n"]
+__all__ = ["logger"]
 
-LOG_DIR = os.environ.get("LOG_DIR", "logs")
-if not LOG_DIR:
-    LOG_DIR = os.path.join(os.path.expanduser("~"), ".shiertier", "logs")
-makedirs(LOG_DIR, exist_ok=True)
-log_file_path = os.path.join(LOG_DIR, "file_{time}.log")
+class EZLogger:
+    def __init__(self,
+                 log_level: str = None, 
+                 log_dir: str = None,
+                 log_sys: bool = None,
+                 *args, **kwargs):
+        
+        self.log_level = log_level or environ.get("LOG_LEVEL", "INFO")
+        self.log_dir = log_dir or environ.get("LOG_DIR", os.path.join(os.path.expanduser("~"), ".cache", "shiertier", "logs"))
+        self.log_sys = log_sys if log_sys is not None else environ.get("LOG_SYS", "True").lower() in ["1", "true"]
 
-LOG_LEVEL_STR = os.environ.get("LOG_LEVEL", "INFO")
-LOG_SYS_BOOL = os.environ.get("LOG_SYS", "True").lower() in ["1", "true"]
+        makedirs(self.log_dir, exist_ok=True)
+        
+        log_file_path = os.path.join(self.log_dir, "file_{time}.log")
+        logger.add(log_file_path, rotation="1 MB", level=self.log_level)
+        
+        if self.log_sys:
+            logger.add(sys.stdout, level=self.log_level)
 
-logger.remove()
-logger.add(log_file_path, rotation="1 MB", level=LOG_LEVEL_STR)  # 1MB for each file
+    def __getattr__(self, name):
+        return getattr(logger, name)
 
-if LOG_SYS_BOOL:
-    logger.add(sys.stdout, level=LOG_LEVEL_STR)
+ez_logger = EZLogger()
 
-try:
-    from shiertier_i18n import i18n
-except ImportError:
-    logger.warning("shiertier_i18n not found, can't use translation support")
-    def i18n(message: str, replace_dict: Dict[str, Any] = None) -> str:
-        if replace_dict:
-            for key, value in replace_dict.items():
-                message = message.replace(key, value)
-        return message
+# TODO
 
-class Logger_I18n:
-    def __init__(self, base_logger, i18n_func):
-        self.base_logger = base_logger
-        self.i18n_func = i18n_func
+# try:
+#     from shiertier_i18n import i18n
+# except ImportError:
+#     logger.warning("shiertier_i18n not found, can't use translation support")
+#     def i18n(message: str, replace_dict: Dict[str, Any] = None) -> str:
+#         if replace_dict:
+#             for key, value in replace_dict.items():
+#                 message = message.replace(key, value)
+#         return message
 
-    def info(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
-        """Logs an info message with translation support."""
-        translated_message = self.i18n_func(message, replace_dict)
-        self.base_logger.info(translated_message)
+# class Logger_I18n:
+#     def __init__(self, base_logger, i18n_func):
+#         self.base_logger = base_logger
+#         self.i18n_func = i18n_func
 
-    def debug(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
-        """Logs a debug message with translation support."""
-        translated_message = self.i18n_func(message, replace_dict)
-        self.base_logger.debug(translated_message)
+#     def info(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
+#         """Logs an info message with translation support."""
+#         translated_message = self.i18n_func(message, replace_dict)
+#         self.base_logger.info(translated_message)
 
-    def warning(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
-        """Logs a warning message with translation support."""
-        translated_message = self.i18n_func(message, replace_dict)
-        self.base_logger.warning(translated_message)
+#     def debug(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
+#         """Logs a debug message with translation support."""
+#         translated_message = self.i18n_func(message, replace_dict)
+#         self.base_logger.debug(translated_message)
 
-    def error(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
-        """Logs an error message with translation support."""
-        translated_message = self.i18n_func(message, replace_dict)
-        self.base_logger.error(translated_message)
+#     def warning(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
+#         """Logs a warning message with translation support."""
+#         translated_message = self.i18n_func(message, replace_dict)
+#         self.base_logger.warning(translated_message)
 
-easy_logger_i18n = Logger_I18n(logger, i18n)
+#     def error(self, message: str, replace_dict: Dict[str, Any] = None) -> None:
+#         """Logs an error message with translation support."""
+#         translated_message = self.i18n_func(message, replace_dict)
+#         self.base_logger.error(translated_message)
+
+# easy_logger_i18n = Logger_I18n(logger, i18n)
